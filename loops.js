8173e6612147a0a4f8761bf3f379670e8b779df9
Danielle Bennett <daniellebennettchan@gmail.com>
2017-06-17 19:03:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  var i;
+  if (i === 1) {
+    return "I am 1 strange loop.";
+} else {
+    for (var i = 0; i < 25; i++);
+    return "I am" + i + " strange loops.";
+  }
+}
+
+function whileLoop(n) {
+  var n = 0;
+  while (n > 0) {
+    console.log(--countdown);
+    n++;
+  }
+  return 'done';
+}
