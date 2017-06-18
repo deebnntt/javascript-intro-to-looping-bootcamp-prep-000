0a8f54fc68250c7264ae16aa6a56913836546b33
Danielle Bennett <daniellebennettchan@gmail.com>
2017-06-18 15:46:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,18 +1,28 @@
 function forLoop(array) {
-  var i;
+  for (let i = 0; i < 25; i++) {
   if (i === 1) {
-    return "I am 1 strange loop.";
+    array.push("I am 1 strange loop.");
 } else {
-    for (var i = 0; i < 25; i++);
-    return "I am" + i + " strange loops.";
+    array.push("I am " + i + " strange loops.");
   }
+}
+  return array;
 }
 
 function whileLoop(n) {
-  var n = 0;
   while (n > 0) {
-    console.log(--countdown);
-    n++;
+    console.log(n);
+    n--;
   }
   return 'done';
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+  return array
 }
